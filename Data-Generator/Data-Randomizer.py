b4e97310274fdcdca62f18557d90802a828655bd
khubaibalam2000 <khubaibalam2000@gmail.com>
2022-11-23 22:35:00 +0500

    more data random

--- Data-Generator/Data-Randomizer.py
+++ Data-Generator/Data-Randomizer.py
@@ -1,4 +1,7 @@
 import random as rn
+import pandas as pd
+from random import randrange
+from datetime import timedelta
 
 def readFile(fileName, listToStore):
     with open(fileName) as stream:
@@ -17,7 +20,7 @@ def getRange(totalTerms, totalData):
             randRange.append(found)
             i+=1
 
-    return ranges, randRange
+    return randRange
 
 allergens = []
 diagnosis = []
@@ -35,4 +38,28 @@ readFile('./Data-Files/Temp-Data/TempTreatments.txt', treatments)
 readFile('./Data-Files/Temp-Data/TempMedicines.txt', medicines)
 readFile('./Data-Files/Temp-Data/TempImmunizations.txt', immunizations)
 
-print(allergens, diagnosis, vital_signs, surgeries, treatments, medicines, immunizations)
+
+def makeRandomValues(data, totalTerms, totalData):
+    finalData = []
+    for i in range(1000):
+        ranger = getRange(totalTerms, totalData)
+        tempData = []
+        for j in range(len(ranger)):
+            tempData.append(data[ranger[j]])
+        finalData.append(tempData)
+    return finalData
+
+
+# def randomVitalSigns():
+#     for i in range(1000):
+
+        
+
+csvAllergens = makeRandomValues(allergens, 4, len(allergens) - 1)
+csvDiagnosis = makeRandomValues(diagnosis, 3, len(diagnosis) - 1)
+# csvVitalSigns = makeRandomValues(vital_signs, 3, len(vital_signs) - 1)
+csvSurgeries = makeRandomValues(surgeries, 2, len(surgeries) - 1)
+csvTreatments = makeRandomValues(treatments, 3, len(treatments) - 1)
+csvMedicines = makeRandomValues(medicines, 15, len(medicines) - 1)
+csvImmunizations = makeRandomValues(immunizations, 2, len(immunizations) - 1)
+
